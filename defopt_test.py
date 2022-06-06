9cdb49adf466ad535834296e97bd480bc4e3daf6
Maxime RIO <maxime.rio@niwa.co.nz>
2022-06-07 00:27:55 +1200

    monkey patch argparser to inject code before parsing

--- defopt_test.py
+++ defopt_test.py
@@ -1,7 +1,15 @@
 import typing as T
 import json
+import argparse
 
+import wrapt
 import defopt
+
+
+@wrapt.patch_function_wrapper(argparse.ArgumentParser, "parse_args")
+def parse_args(wrapped, instance, args, kwargs):
+    print("pouet", flush=True)
+    return wrapped(*args, **kwargs)
 
 
 def main(*, params: T.Dict[str, T.Any]):
